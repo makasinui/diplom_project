245fbe7b3f51c32814a1733b3389da9f4ff5ecf6
makasinui <mks.shi@ya.ru>
2023-02-18 02:50:33 +0300

    Добавил модель товаров, добавил возможность получать их из запроса

---
+++ backend/app/Http/Requests/ProductsStoreRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProductsStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|max:150',
+            'description' => 'max:500',
+            'price' => 'required',
+            'vin' => 'required',
+            'img' => 'max:1000'
+        ];
+    }
+}
