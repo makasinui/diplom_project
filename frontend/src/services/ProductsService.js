9b4a35dd7a675a4fa6837f0c0c3a63189ac1e7e5
makasinui <mks.shi@ya.ru>
2023-02-27 00:22:26 +0300

    Добавил метод getPopular который выводит товары на главной

--- frontend/src/services/ProductsService.js
+++ frontend/src/services/ProductsService.js
@@ -1,10 +1,14 @@
-import HttpService from "./HttpService";
+import HttpService from "./HttpService"
 
 export default class ProductsService {
   url = "http://localhost:8000/api/products";
   httpService = new HttpService();
 
   getAll() {
+    return this.httpService.get(this.url).then(({ data }) => data);
+  }
+
+  getPopular() {
     return this.httpService.get(this.url).then(({ data }) => data);
   }
 }
