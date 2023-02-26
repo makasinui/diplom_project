13ac0ba0fe5105c8b52fc0475c3a1626307c4476
makasinui <mks.shi@ya.ru>
2023-02-27 00:21:45 +0300

    Добавил второстепенные страницы

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,16 +1,34 @@
+import { createWebHistory, createRouter } from "vue-router";
+
 import App from "../App.vue";
 import Main from "../pages/Main.vue";
-import { createWebHistory, createRouter } from "vue-router";
 import Catalog from "../pages/Catalog.vue";
+import AboutUs from '../pages/AboutUs.vue';
+import MainAdmin from '../pages/admin/MainAdmin.vue';
+import Clients from '../pages/Clients.vue';
+import Contacts from '../pages/Contacts.vue';
 
 const routes = [
   { path: "/", component: Main },
   { path: "/catalog", component: Catalog },
+  { path: "/about-us", component: AboutUs },
+  { path: "/clients", component: Clients },
+  { path: "/contacts", component: Contacts },
+  { path: "/admin", component: MainAdmin }
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
 });
+
+router.beforeEach((to, from, next) => {
+  if (to?.meta?.forAdmin) {
+    if(localStorage.getItem('authToken')) {
+      next()
+    }
+  }
+  next();
+})
 
 export default router;
