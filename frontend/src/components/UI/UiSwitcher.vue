3030190047abf01332eea6e50e93f09e9b191ba4
makasinui <mks.shi@ya.ru>
2023-05-11 21:57:25 +0300

    Добавлен свитчер

---
+++ frontend/src/components/UI/UiSwitcher.vue
@@ -0,0 +1,74 @@
+<template>
+    <label class="checkbox">
+        <input
+            class="checkbox-popular"
+            type="checkbox"
+            :checked="value"
+            @change="(ev) => emit('change', ev.target.checked)"
+        />
+        <span class="checkbox-switch"></span>
+    </label>
+</template>
+
+<script setup>
+defineProps({ value: Boolean });
+const emit = defineEmits(["change"]);
+</script>
+
+<style lang="scss" scoped>
+.checkbox {
+    display: inline-block;
+    height: 28px;
+    line-height: 28px;
+    margin-right: 10px;
+    position: relative;
+    vertical-align: middle;
+    font-size: 14px;
+    user-select: none;
+
+    &-popular {
+        display: block;
+        width: 0;
+        height: 0;
+        position: absolute;
+        z-index: -1;
+        opacity: 0;
+    }
+
+    &-popular:checked + &-switch {
+        background: #9abef7;
+    }
+
+    &-popular:checked + &-switch:before {
+        background: #3f64ae;
+        transform: translateX(18px);
+    }
+
+    &-switch {
+        display: inline-block;
+        width: 36px;
+        height: 14px;
+        border-radius: 20px;
+        position: relative;
+        top: 6px;
+        vertical-align: top;
+        background: #9f9f9f;
+        transition: 0.2s;
+
+        &:before {
+            content: "";
+            display: inline-block;
+            width: 20px;
+            height: 20px;
+            position: absolute;
+            top: -3px;
+            left: -1px;
+            background: #fff;
+            border-radius: 50%;
+            box-shadow: 0 3px 1px -2px rgba(0, 0, 0, 0.2),
+                0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12);
+            transition: 0.15s;
+        }
+    }
+}
+</style>
--- frontend/src/components/UI/index.js
+++ frontend/src/components/UI/index.js
@@ -1,11 +1,13 @@
 import Button from "./UiButton.vue";
 import Input from "./UiInput.vue";
 import Pagination from './UiPagination.vue';
+import Switcher from './UiSwitcher.vue';
 
 const UIComponents = [
     Button,
     Input,
-    Pagination
+    Pagination,
+    Switcher
 ]
 
 export default UIComponents
