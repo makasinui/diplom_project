06cd0d48743de05fc12c24c102882967b99f3d7d
makasinui <mks.shi@ya.ru>
2023-06-15 20:35:27 +0300

    production backend url hide in env

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,24 +1,29 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  },
-  server: {
-    proxy: {
-      '/backend': {
-        target: 'http://khizauto.backend:8000',
-        changeOrigin: true,
-        secure: false,
-        ws: true,
-        rewrite: (path) => path.replace(/^\/backend/, ''),
+export default ({ mode }) => {
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())}
+  console.log(process.env)
+
+  return defineConfig({
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src')
+      }
+    },
+    server: {
+      proxy: {
+        '/backend': {
+          target: process.env.VITE_BACKEND_URL,
+          changeOrigin: true,
+          secure: false,
+          ws: true,
+          rewrite: (path) => path.replace(/^\/backend/, ''),
+        }
       }
     }
-  }
-})
+  })
+}
